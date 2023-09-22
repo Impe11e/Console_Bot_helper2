97152c24c261489bf530be7a91e9755dd94a5d7a
ariia horunzha <1mpellee@gmail.com>
2023-09-22 13:19:32 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,113 @@
+from collections import UserDict
+
+class Field:
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return str(self.value)
+
+class Name(Field):
+    def __init__(self, name):
+        super().__init__(name)
+class Phone(Field):
+    def __init__(self, phone):
+        if not self.validator(phone):
+            raise ValueError('Incorrect phone format was entered!')
+        super().__init__(phone)
+    def validator(self, phone):
+        return phone.isdigit() and len(phone) == 10
+class Record:
+    def __init__(self, name):
+        self.name = Name(name)
+        self.phones = []
+    def add_phone(self, phone):
+        self.phones.append(phone)
+    def remove_phone(self, phone):
+        if phone in self.phones:
+            self.phones = list(filter(lambda x: x != phone, self.phones))
+        else:
+            raise ValueError('There is no such phone in Records!')
+
+    def edit_phone(self, old_phone, new_phone):
+        if old_phone in self.phones:
+            for i, phone in enumerate(self.phones):
+                if old_phone == phone:
+                    self.phones[i] = new_phone
+        else:
+            raise ValueError
+
+class Record:
+    def __init__(self, name):
+        self.name = Name(name)
+        self.phones = []
+
+    def add_phone(self, phone):
+        phone_instance = Phone(phone)
+        self.phones.append(phone_instance)
+    def remove_phone(self, phone):
+        if isinstance(phone, Phone):
+            if phone in self.phones:
+                self.phones = list(filter(lambda x: x != phone, self.phones))
+            else:
+                raise ValueError('There is no such phone in Records!')
+        else:
+            raise ValueError('Invalid phone format')
+
+    def find_phone(self, phone_number):
+        for phone in self.phones:
+            if phone.value == phone_number:
+                return phone
+        return None
+
+    def edit_phone(self, old_phone, new_phone):
+        old_phone_instance = Phone(old_phone)
+        new_phone_instance = Phone(new_phone)
+
+        if old_phone_instance in self.phones:
+            for i, phone in enumerate(self.phones):
+                if old_phone_instance == phone:
+                    self.phones[i] = new_phone_instance
+        else:
+            return None
+
+class AddressBook(UserDict):
+    def add_record(self, record):
+        self.data[record.name.value] = record
+
+    def find(self, record):
+        if record in self.data.keys():
+            return self.data.get(record)
+
+
+    def delete(self, record):
+        if record in self.data:
+            del self.data[record]
+        # else:
+        #     raise ValueError('No such name')
+
+# # Створення нової адресної книги
+# book = AddressBook()
+# # Створення запису для John
+# john_record = Record("John")
+# john_record.add_phone("1234567890")
+# john_record.add_phone("5555555555")
+# # Додавання запису John до адресної книги
+# book.add_record(john_record)
+# # Створення та додавання нового запису для Jane
+# jane_record = Record("Jane")
+# jane_record.add_phone("9876543210")
+# book.add_record(jane_record)
+# # Виведення всіх записів у книзі
+# for name, record in book.data.items():
+#     print(record)
+# # Знаходження та редагування телефону для John
+# john = book.find("John")
+# john.edit_phone("1234567890", "1112223333")
+# print(john)  # Виведення: Contact name: John, phones: 1112223333; 5555555555
+# # Пошук конкретного телефону у записі John
+# found_phone = john.find_phone("5555555555")
+# print(f"{john.name}: {found_phone}")  # Виведення: 5555555555
+#
+# # Видалення запису Jane
+# book.delete("Jane")
